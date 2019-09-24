ed9c4f318a0e037749ced17720c54f8c541d8e54
Mohd Hisham <breezebwind@yahoo.com>
2019-09-24 10:38:48 +0800

    Create Table Migration

---
+++ readme.md
@@ -0,0 +1,2 @@
+<h1>Customer CRUD</h1>
+<p>This is customer CRUD</p>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('title', 'Welcome')
+
+@section('content')
+    <p>Welcome To Project</p>
+@endsection
